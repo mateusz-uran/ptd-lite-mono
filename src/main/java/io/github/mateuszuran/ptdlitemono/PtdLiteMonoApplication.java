cb66d8831da358e6148641f7644f5d34e8abeaf6
mateusz-uran <mateusz.uranowski@onet.pl>
2023-05-03 08:17:41 +0200

    Init project

---
+++ src/main/java/io/github/mateuszuran/ptdlitemono/PtdLiteMonoApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateuszuran.ptdlitemono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtdLiteMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtdLiteMonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateuszuran/ptdlitemono/PtdLiteMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateuszuran.ptdlitemono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtdLiteMonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
