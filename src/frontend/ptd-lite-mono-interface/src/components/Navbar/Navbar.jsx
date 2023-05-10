e1102e4089ec20be4db96f97e6805323b61d0691
mateusz-uran <mateusz.uranowski@onet.pl>
2023-05-10 11:33:02 +0200

    feat: move theme handler to App.jsx

--- src/frontend/ptd-lite-mono-interface/src/components/Navbar/Navbar.jsx
+++ src/frontend/ptd-lite-mono-interface/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import Switch from '@mui/material/Switch';
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import React from 'react';
 import Divider from '@mui/material/Divider';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from '@mui/material';
@@ -10,34 +6,10 @@ import CardsList from '../CardList/CardsList';
 
 function Navbar(props) {
     const { user, logout, loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
-
-    const [darkMode, setDarkMode] = useState(false);
-
-    const handleChangeTheme = (event) => {
-        setDarkMode(event.target.checked);
-        localStorage.setItem('theme', JSON.stringify(event.target.checked));
-    }
-
-    const darkTheme = createTheme(darkMode ?
-        {
-            palette: {
-                mode: 'dark',
-            },
-        } : {
-            palette: {
-                mode: 'light',
-            }
-        }
-    )
-
-    useEffect(() => {
-        let availableTheme = JSON.parse(localStorage.getItem('theme'));
-        availableTheme && setDarkMode(availableTheme);
-    }, []);
+    const { themeMode } = props;
 
     return (
-        <div className={`${darkMode ? 'dark bg-slate-700' : 'bg-blue-200'}`}>
-            <ThemeProvider theme={darkTheme}>
+        <div className={`${themeMode ? 'dark bg-slate-700' : 'bg-blue-200'}`}>
                 <div className='p-4 flex justify-between items-center'>
                     <div className='flex'>
                         &nbsp;
@@ -53,24 +25,15 @@ function Navbar(props) {
                                 <Button onClick={() => logout()} variant="contained" sx={{ fontWeight: 'bold' }}>Logout</Button>
                             }
                         </div>
-                        <div className='flex items-center'>
-                            <WbSunnyIcon className={darkMode ? 'text-blue-200' : 'text-white'} />
-                            <Switch
-                                checked={darkMode}
-                                onChange={handleChangeTheme}
-                            />
-                            <DarkModeIcon className={darkMode ? 'text-white' : ''} />
-                        </div>
                     </div>
                 </div>
                 <Divider sx={{ borderBottomWidth: 2, marginBottom: 0 }} />
                 {
                     isAuthenticated && user.nickname &&
                     <div>
-                        <CardsList mode={darkMode} user={user.nickname} />
+                        <CardsList mode={themeMode} user={user.nickname} />
                     </div>
                 }
-            </ThemeProvider >
         </div >
     );
 }
