dbc84e13e842c33f545d744d0d49ea3518e4cb49
mateusz-uran <mateusz.uranowski@onet.pl>
2023-05-23 13:29:51 +0200

    refactor: move input and validations array to single file

--- src/frontend/ptd-lite-mono-interface/src/components/Fuel/forms/AddAdBlue.jsx
+++ src/frontend/ptd-lite-mono-interface/src/components/Fuel/forms/AddAdBlue.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import * as Yup from 'yup';
 import { Button } from '@mui/material';
 import useFuelService from '../../../api/FuelService/FuelServiceHook';
 import { useTranslation } from 'react-i18next';
 import FuelForm from './FuelForm';
+import { inputsAdBlue, validationSchemaAdBlue } from './inputs';
 
 function AddAdBlue(props) {
     const { t } = useTranslation();
@@ -12,24 +12,12 @@ function AddAdBlue(props) {
 
     const navigate = useNavigate();
 
-    const inputs = [
-        { name: 'date', label: t('addFuel.date') },
-        { name: 'localization', label: t('addFuel.location') },
-        { name: 'amount', label: t('addFuel.amount') },
-    ];
-
-    const validation = Yup.object({
-        date: Yup.string().required(t('yup.empty')),
-        localization: Yup.string().required(t('yup.empty')),
-        amount: Yup.string().required(t('yup.empty')),
-    });
-
     return (
         <div className='flex flex-col'>
             <div className='text-left p-2'>
                 <Button variant="outlined" onClick={() => navigate(-1)} sx={{ fontWeight: 'bold' }}>{t('misc.goBackButton')}</Button>
             </div>
-            <FuelForm service={createAdBlue} inputs={inputs} validations={validation} button={t('addFuel.submit')} />
+            <FuelForm service={createAdBlue} inputs={inputsAdBlue} validations={validationSchemaAdBlue} button={t('addFuel.submit')} />
         </div>
     );
 }
