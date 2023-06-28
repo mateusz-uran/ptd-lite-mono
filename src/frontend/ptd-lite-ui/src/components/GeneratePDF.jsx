a909022f002a393339bbae3ca5cbbb7f960a498c
mateusz-uran <mateusz.uranowski@onet.pl>
2023-06-28 17:25:22 +0200

    feat: create async thunk to generate pdf

--- src/frontend/ptd-lite-ui/src/components/GeneratePDF.jsx
+++ src/frontend/ptd-lite-ui/src/components/GeneratePDF.jsx
@@ -1,65 +1,38 @@
-import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getAdditionalInfo } from '../features/additionalInfo/additionalInfoSlice';
-import { getAccessToken } from '../features/auth/auth0Slice';
 import { useParams } from 'react-router-dom';
-import { useState } from 'react';
 import ProgressModal from '../features/pdf/ProgressModal';
 import { useTranslation } from 'react-i18next';
-
-const API_URL = import.meta.env.VITE_BACKEND_API_URL;
-const FRONTEND_URL = import.meta.env.VITE_FRONT_URL;
+import { generatePdf, selectIsLoading } from '../api/pdf/pdfApiSlice';
 
 const GeneratePDF = () => {
   const { t } = useTranslation();
   const { user } = useAuth0();
   const { cardId } = useParams();
-  const accessToken = useSelector(getAccessToken);
+  const dispatch = useDispatch();
   const additionalInfo = useSelector(getAdditionalInfo);
-  const [showProgressModal, setShowProgressModal] = useState(false);
 
-  const generatePDF = async (page) => {
-    setShowProgressModal(true);
-    const username = user.nickname;
-    const url = `${API_URL}/pdf/generate-doc`;
-    try {
-      const response = await axios.post(url, additionalInfo, {
-        headers: {
-          'Access-Control-Allow-Origin': FRONTEND_URL,
-          Authorization: `Bearer ${accessToken}`,
-        },
-        params: {
-          username,
-          cardId,
-          page,
-        },
-        responseType: 'blob',
-      });
+  const isLoading = useSelector(selectIsLoading);
 
-      const blob = new Blob([response.data], { type: 'application/pdf' });
-      const objectURL = URL.createObjectURL(blob);
-
-      window.open(objectURL, '_blank');
-
-      URL.revokeObjectURL(objectURL);
-      setShowProgressModal(false);
-    } catch (error) {
-      console.error('Error generating PDF:', error);
-      setShowProgressModal(false);
-    }
+  const generatePDFAndShow = async (page) => {
+    let username = user.nickname;
+    dispatch(generatePdf({ username, cardId, page, additionalInfo }));
   };
 
   return (
     <div className="pdf-button-wrapper">
-      <button className="primary-btn pdf-button" onClick={() => generatePDF()}>
+      <button
+        className="primary-btn pdf-button"
+        onClick={() => generatePDFAndShow()}
+      >
         {t('buttons.downloadPdf')}
       </button>
       <ul>
         <li>
           <button
             className="small-btn list-btn"
-            onClick={() => generatePDF('first')}
+            onClick={() => generatePDFAndShow('first')}
           >
             {t('buttons.downloadPdfFirst')}
           </button>
@@ -67,13 +40,13 @@ const GeneratePDF = () => {
         <li>
           <button
             className="small-btn list-btn"
-            onClick={() => generatePDF('second')}
+            onClick={() => generatePDFAndShow('second')}
           >
             {t('buttons.downloadPdfSecond')}
           </button>
         </li>
       </ul>
-      {showProgressModal && <ProgressModal />}
+      {isLoading && <ProgressModal />}
     </div>
   );
 };
