c10b6be116e7cc4bbf34cd23b17b061f31fe3db8
mateusz-uran <mateusz.uranowski@onet.pl>
2023-06-29 16:44:26 +0200

    fix: move to browserouter and update auth context

--- src/frontend/ptd-lite-ui/src/features/auth/Auth0Wrapper.jsx
+++ src/frontend/ptd-lite-ui/src/features/auth/Auth0Wrapper.jsx
@@ -1,20 +1,28 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { setAuthContext } from './auth0Slice';
+import Loading from './Loading';
 
 const Auth0Wrapper = ({ children }) => {
   const dispatch = useDispatch();
   const auth0 = useAuth0();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchAccessToken = async () => {
-      const token = await auth0.getAccessTokenSilently();
-      dispatch(setAuthContext(token));
+      try {
+        const token = await auth0.getAccessTokenSilently();
+        dispatch(setAuthContext(token));
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
     };
-    fetchAccessToken();
-  }, [auth0]);
 
-  return children;
+    fetchAccessToken();
+  }, [auth0, dispatch]);
+
+  return loading ? <Loading /> : children;
 };
 export default Auth0Wrapper;
---
+++ src/frontend/ptd-lite-ui/src/features/auth/CallbackPage.jsx
@@ -0,0 +1,10 @@
+import Sidebar from '../../components/Sidebar';
+
+const CallbackPage = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+};
+export default CallbackPage;
