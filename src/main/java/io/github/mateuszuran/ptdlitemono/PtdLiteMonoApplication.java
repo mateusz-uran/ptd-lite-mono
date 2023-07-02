2b785419bbc5b37e2b201ffad27f5af0d8ffcabc
mateusz-uran <mateusz.uranowski@onet.pl>
2023-07-02 09:54:15 +0200

    refactor: remove test data from runtime class

--- src/main/java/io/github/mateuszuran/ptdlitemono/PtdLiteMonoApplication.java
+++ src/main/java/io/github/mateuszuran/ptdlitemono/PtdLiteMonoApplication.java
@@ -1,41 +1,12 @@
 package io.github.mateuszuran.ptdlitemono;
 
-import io.github.mateuszuran.ptdlitemono.model.Card;
-import io.github.mateuszuran.ptdlitemono.repository.CardRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @SpringBootApplication
-@RequiredArgsConstructor
-public class PtdLiteMonoApplication implements CommandLineRunner {
-    private final CardRepository repository;
+public class PtdLiteMonoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PtdLiteMonoApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        loadTestData();
-    }
-
-    private void loadTestData() {
-        // Create and save entities
-        String username = "test123";
-        Card card1 = Card.builder().number(username).username(username).creationTime(LocalDateTime.now()).build();
-        Card card2 = Card.builder().number(username).username(username).creationTime(LocalDateTime.of(2023, 6, 30, 12, 0)).build();
-        Card card3 = Card.builder().number(username).username(username).creationTime(LocalDateTime.of(2023, 6, 25, 16, 35)).build();
-        Card card4 = Card.builder().number(username).username(username).creationTime(LocalDateTime.of(2023, 6, 15, 8, 59)).build();
-        Card card5 = Card.builder().number(username).username(username).creationTime(LocalDateTime.of(2023, 4, 3, 21, 59)).build();
-        Card card6 = Card.builder().number(username).username(username).creationTime(LocalDateTime.of(2023, 4, 19, 13, 17)).build();
-        Card card7 = Card.builder().number(username).username(username).creationTime(LocalDateTime.of(2022, 10, 8, 18, 48)).build();
-        Card card8 = Card.builder().number(username).username(username).creationTime(LocalDateTime.of(2022, 12, 23, 9, 15)).build();
-        Card card9 = Card.builder().number(username).username(username).creationTime(LocalDateTime.of(2022, 7, 11, 15, 39)).build();
-        // Additional data loading logic if needed
     }
 }
